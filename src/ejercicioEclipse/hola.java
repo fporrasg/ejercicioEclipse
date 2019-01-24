22acce8dcb048c4023590403d9200366bedc2704
Francisco Porras Garcia <fporras@viewnext.com>
2019-01-24 18:20:11 +0100

    Muy bien

--- src/ejercicioEclipse/hola.java
+++ src/ejercicioEclipse/hola.java
@@ -6,7 +6,8 @@ public class hola {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola"); 
-		System.out.println("Que tal?"); 
+		System.out.println("Que tal?");
+		System.out.println("Muy bien"); 
 	}
 	
 	
